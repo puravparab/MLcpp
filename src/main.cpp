8893a6acbe7c6c88e619ca413997c6ded3876b28
PuravParab <47409476+PuravParab@users.noreply.github.com>
2023-02-10 03:31:01 +0400

    made example functions

--- src/main.cpp
+++ src/main.cpp
@@ -10,59 +10,10 @@
 
 using Eigen::MatrixXd;
 
-void display_data(MatrixXd x_train, MatrixXd y_train, 
-	MatrixXd weights, double bias, double learning_rate){
-		std::cout << "\nx_train = " << std::endl << x_train << "\n" << std::endl; 
-		std::cout << "y_train = " << std::endl << y_train << "\n" << std::endl; 
-		std::cout << "weights = " "[" << weights.transpose() << "]" << std::endl; 
-		std::cout << "bias = " << bias << std::endl;
-		std::cout << "learning rate = " << learning_rate << "\n" << std::endl; 
-}
+// Examples
 
-void display_results(MatrixXd y_predict, MatrixXd y_train){
-	std::cout << "\nTraining Complete.\n" << std::endl <<
-	"prediction: [" << y_predict.transpose() << "]" << std::endl <<
-	"training: [" << y_train.transpose() << "]" << std::endl;
-}
-
-int main()
-{
-	std::cout << "MLcpp: Machine Learning library built with C++" << std::endl;
-
-	// // Logistic Regression:
-	// std::string url = ".\\dataset\\binary_test1.csv";
-	// Dataset data(url);
-	// MatrixXd train = data.get_train();
-	// MatrixXd test = data.get_test();
-	// MatrixXd x_train = train.block(0, 0, train.rows(), train.cols() - 1);
-	// MatrixXd y_train = train.col(train.cols() - 1);
-	// MatrixXd x_test = test.block(0, 0, test.rows(), test.cols()-1);
-	// MatrixXd y_test = test.col(test.cols()-1);
-
-	// // std::cout << "training examples: " << x_train.rows() << " test examples: " << x_test.rows() << std::endl;
-	// MatrixXd weights{
-	// 	{0},{0}
-	// };
-	// double bias = 0;
-	// double learning_rate = 0.3;
-	// double epsilon = 1e-6;
-	// double iterations = 5000;
-
-	// // Train logistic regression model
-	// Logistic logistic(x_train, y_train, weights, bias);
-	// MatrixXd y_predict = logistic.train(x_test, y_test, learning_rate, "bgd", epsilon, iterations, 200);
-
-	// std::cout << "\nTest Evaluation Loss:\n" << logistic.evaluate(x_test, y_test) << std::endl;
-	// std::cout << "\nPredictions: \n" << y_predict << std::endl;
-
-	// MatrixXd x{
-	// 	{2.5, 3},
-	// 	{0.5, 0.5}
-	// };
-	// std::cout << "\nPredictions:\n" << logistic.predict(x) << std::endl;
-
-
-	// Multiple Regression:
+// Multiple Regression using real_estate.csv dataset
+void example_linear_regression(){
 	std::string url = ".\\dataset\\real_estate.csv";
 	Dataset dataset(url, 80);
 	MatrixXd train = dataset.get_train();
@@ -109,4 +60,63 @@ int main()
 	};
 	x = normalized.process(x);
 	std::cout << "Predictions: \n" << linear.predict(x) * scale << " dollars" << std::endl;
+}
+
+// Logistic Regression using binary_test1.csv dataset
+void example_logistic_regression(){
+	std::string url = ".\\dataset\\binary_test1.csv";
+	Dataset data(url);
+	MatrixXd train = data.get_train();
+	MatrixXd test = data.get_test();
+	MatrixXd x_train = train.block(0, 0, train.rows(), train.cols() - 1);
+	MatrixXd y_train = train.col(train.cols() - 1);
+	MatrixXd x_test = test.block(0, 0, test.rows(), test.cols()-1);
+	MatrixXd y_test = test.col(test.cols()-1);
+
+	std::cout << "training examples: " << x_train.rows() << " test examples: " << x_test.rows() << std::endl;
+	MatrixXd weights{
+		{0},{0}
+	};
+	double bias = 0;
+	double learning_rate = 0.3;
+	double epsilon = 1e-6;
+	double iterations = 5000;
+
+	// Train logistic regression model
+	Logistic logistic(x_train, y_train, weights, bias);
+	MatrixXd y_predict = logistic.train(x_test, y_test, learning_rate, "bgd", epsilon, iterations, 200);
+
+	std::cout << "\nTest Evaluation Loss:\n" << logistic.evaluate(x_test, y_test) << std::endl;
+	std::cout << "\nPredictions: \n" << y_predict << std::endl;
+
+	MatrixXd x{
+		{2.5, 3},
+		{0.5, 0.5}
+	};
+	std::cout << "\nPredictions:\n" << logistic.predict(x) << std::endl;
+}
+
+// Display functions
+
+void display_data(MatrixXd x_train, MatrixXd y_train, 
+	MatrixXd weights, double bias, double learning_rate){
+		std::cout << "\nx_train = " << std::endl << x_train << "\n" << std::endl; 
+		std::cout << "y_train = " << std::endl << y_train << "\n" << std::endl; 
+		std::cout << "weights = " "[" << weights.transpose() << "]" << std::endl; 
+		std::cout << "bias = " << bias << std::endl;
+		std::cout << "learning rate = " << learning_rate << "\n" << std::endl; 
+}
+
+void display_results(MatrixXd y_predict, MatrixXd y_train){
+	std::cout << "\nTraining Complete.\n" << std::endl <<
+	"prediction: [" << y_predict.transpose() << "]" << std::endl <<
+	"training: [" << y_train.transpose() << "]" << std::endl;
+}
+
+int main()
+{
+	std::cout << "MLcpp: Machine Learning library built with C++" << std::endl;
+
+	// example_linear_regression();
+	// example_logistic_regression();
 }
