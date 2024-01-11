c30e6c072d7fe4213174be634ebf6e2d413109bf
Purav <puravparabdev@gmail.com>
2024-01-11 06:38:49 -0500

    updated logistic_regression example to use appropriate gradient function

--- examples/logistic_regression/main.cpp
+++ examples/logistic_regression/main.cpp
@@ -11,17 +11,40 @@ Eigen::VectorXf sigmoid(Eigen::VectorXf X){
 	return 1.0 / (1.0 + (-X.array()).exp());
 }
 
-void logistic_regression(Eigen::MatrixXf&X, Eigen::MatrixXf& Y, Eigen::VectorXf& W, float& B, float lr, int epochs){
+void logistic_regression_bgd(Eigen::MatrixXf&X, Eigen::MatrixXf& Y, Eigen::VectorXf& W, float& B, float lr, int epochs){
+	int steps;
+  if (epochs > 10){
+    steps = epochs / 10;
+  } else {
+    steps = epochs;
+  }
 	Eigen::VectorXf history;
 	printf("\nBatch gradient descent\n");
 	printf("Training starting...\n");
 	for (int i = 0; i < epochs; i++){
-    history = batchgd(X, Y, W, B, lr, bce);
-    std::cout << "Epoch: " << i + 1 << " " << "loss: " << history[history.rows() - 1] << std::endl;
+    history = batchgd_logistic(X, Y, W, B, lr, bce);
+		if (i % steps == 10){
+    	std::cout << "Epoch: " << i + 1 << " " << "loss: " << history[history.rows() - 1] << std::endl;
+		}
     W = history.head(X.cols());
     B = history[X.cols()];
   }
   std::cout << "Epoch: " << epochs << " " << "loss: " << history[history.rows() - 1] << std::endl;
+}
+
+void logistic_regression_mbgd(Eigen::MatrixXf&X, Eigen::MatrixXf& Y, Eigen::VectorXf& W, float& B, float lr, int epochs, int batch){
+	int steps;
+  if (epochs > 10){ steps = epochs / 10;} 
+	else {steps = epochs;}
+	Eigen::MatrixXf history;
+	printf("\nMini-Batch gradient descent\n");
+	printf("Training starting...\n");
+  for (int i = 0; i < epochs; i++){
+    history = mbgd(X, Y, W, B, lr, mse, batch);
+    std::cout << "Epoch: " << i + 1 << " " << "loss: " << history(history.rows() - 1, history.cols() - 1) << std::endl;
+    W = history.block(history.rows() - 1, 0, 1, W.rows()).transpose();
+    B = history(history.rows() - 1, history.cols() - 2);
+  }
 }
 
 int main(){
@@ -59,19 +82,53 @@ int main(){
 
 	// run logistic regression
 	// batch gradient descent
-  float learning_rate = 0.001;
-  int epochs = 80;
+  float learning_rate = 0.01;
+  int epochs = 10000;
   Eigen::VectorXf W_1 = Eigen::VectorXf::Zero(x_train.cols());
   float B_1 = 1;
-  logistic_regression(x_train, y_train, W_1, B_1, learning_rate, epochs);
+  logistic_regression_bgd(x_train, y_train, W_1, B_1, learning_rate, epochs);
 	Eigen::VectorXf test_pred = sigmoid(x_test * W_1 + Eigen::VectorXf::Constant(x_test.rows(), B_1));
 	float test_loss_1 = bce(test_pred, y_test);
 	Eigen::VectorXf weights_1(W_1.size() + 1);
 	weights_1 << W_1, B_1;
-	save_weights(weights_1, W_1.size(), "json", "weights.json");
+	save_weights(weights_1, W_1.size(), "json", "b_weights.json");
+
+	// mini-batch gradient descent
+	// float learning_rate = 0.001;
+  // int epochs = 80;
+  // Eigen::VectorXf W_2 = Eigen::VectorXf::Zero(x_train.cols());
+  // float B_2 = 1;
+  // logistic_regression(x_train, y_train, W_2, B_2, learning_rate, epochs);
+	// Eigen::VectorXf test_pred = sigmoid(x_test * W_2 + Eigen::VectorXf::Constant(x_test.rows(), B_2));
+	// float test_loss_2 = bce(test_pred, y_test);
+	// Eigen::VectorXf weights_2(W_2.size() + 1);
+	// weights_2 << W_2, B_2;
+	// save_weights(weights_2, W_2.size(), "json", "mb_weights.json");
+
+	// stochastic gradient descent
+	// float learning_rate = 0.001;
+  // int epochs = 80;
+  // Eigen::VectorXf W_3 = Eigen::VectorXf::Zero(x_train.cols());
+  // float B_3 = 1;
+  // logistic_regression(x_train, y_train, W_3, B_3, learning_rate, epochs);
+	// Eigen::VectorXf test_pred = sigmoid(x_test * W_3 + Eigen::VectorXf::Constant(x_test.rows(), B_1));
+	// float test_loss_3 = bce(test_pred, y_test);
+	// Eigen::VectorXf weights_3(W_3.size() + 1);
+	// weights_3 << W_3, B_3;
+	// save_weights(weights_3, W_3.size(), "json", "s_weights.json");
 
 	std::cout<< "\nBatch gradient descent:" << std::endl;
 	std::cout << "Weights: " << W_1.transpose().format(Eigen::IOFormat(Eigen::StreamPrecision, 0, ", ", "\n", "[", "]")) << std::endl;
 	std::cout << "Bias: " << B_1 << std::endl;
 	std::cout << "Test loss: " << test_loss_1 << std::endl;
+
+	// std::cout<< "\nMini-Batch gradient descent:" << std::endl;
+	// std::cout << "Weights: " << W_2.transpose().format(Eigen::IOFormat(Eigen::StreamPrecision, 0, ", ", "\n", "[", "]")) << std::endl;
+	// std::cout << "Bias: " << B_2 << std::endl;
+	// std::cout << "Test loss: " << test_loss_2 << std::endl;
+
+	// std::cout<< "\nStochastic gradient descent:" << std::endl;
+	// std::cout << "Weights: " << W_3.transpose().format(Eigen::IOFormat(Eigen::StreamPrecision, 0, ", ", "\n", "[", "]")) << std::endl;
+	// std::cout << "Bias: " << B_3 << std::endl;
+	// std::cout << "Test loss: " << test_loss_3 << std::endl;
 }
